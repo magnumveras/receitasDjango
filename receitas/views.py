ede6f93c4ce5ccdfd50fe3b942f9465e6109c9ed
magno.veras88@gmail.com <mg338821*#!>
2022-02-15 15:58:35 -0300

    Inclusão de templates e arquivos estáticos.

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Receitas</h1> <h2>Bem Vindo</h2>')
+    return render(request, 'index.html')
